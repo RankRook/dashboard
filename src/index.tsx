f3187364c0da03009507a27283ccd41280d35e23
hienhoang123 <rankrook@gmail.com>
2023-03-13 22:28:14 +0700

    finsh-project

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './assets/css/App.css';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import AdminLayout from './layouts/admin'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme/theme';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+	<ChakraProvider theme={theme}>
+		<React.StrictMode>
+			<HashRouter>
+				<Switch>
+					<Route path={`/admin`} component={AdminLayout} />
+					<Redirect from='/' to='/admin' />
+				</Switch>
+			</HashRouter>
+		</React.StrictMode>
+	</ChakraProvider>,
+	document.getElementById('root')
+);
